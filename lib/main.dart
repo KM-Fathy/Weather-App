1f64bde1f37355b3fb6db4f4258580a103e4964d
KM-Fathy <kareem.eissa88@gmail.com>
2026-09-11 04:52:30 +0300

    setup App

--- lib/main.dart
+++ lib/main.dart
@@ -10,6 +10,9 @@ class WeatherApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const MaterialApp(home: HomeView());
+    return const MaterialApp(
+      debugShowCheckedModeBanner: false,
+      home: HomeView(),
+    );
   }
 }
