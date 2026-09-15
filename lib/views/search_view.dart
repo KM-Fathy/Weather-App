ca45763f4f20194895e7b174ff0937ac0fd65d62
KM-Fathy <kareem.eissa88@gmail.com>
2026-09-15 11:08:20 +0300

    trigger get weather cubit

--- lib/views/search_view.dart
+++ lib/views/search_view.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:weather_app/cubits/get_weather_cubit/get_weather_cubit.dart';
 
 class SearchView extends StatelessWidget {
   const SearchView({super.key});
@@ -24,6 +26,9 @@ class SearchView extends StatelessWidget {
         child: Center(
           child: TextField(
             onSubmitted: (value) {
+              var getWeatherCubit = BlocProvider.of<GetWeatherCubit>(context);
+              getWeatherCubit.getCurrentWeather(cityName: value);
+
               Navigator.pop(context);
             },
             decoration: const InputDecoration(
