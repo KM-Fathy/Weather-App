35352369ccaa95111c88a1695d391d1be0e9da10
KM-Fathy <kareem.eissa88@gmail.com>
2026-09-17 10:37:53 +0300

    fix date

--- lib/widgets/weather_info_body.dart
+++ lib/widgets/weather_info_body.dart
@@ -40,7 +40,7 @@ class WeatherInfoBody extends StatelessWidget {
               ),
             ),
             Text(
-              "updated at: ${weatherModel.date.hour}:${weatherModel.date.minute}${weatherModel.date.second}",
+              "updated at: ${weatherModel.date.hour}:${weatherModel.date.minute.toString().padLeft(2, '0')}",
               style: const TextStyle(
                 fontSize: 24,
               ),
